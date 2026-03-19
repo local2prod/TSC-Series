e21051bf47cac2bf71249a90ca14c8c876bec5e9
Mayank Yadav <mayanky975@gmail.com>
2026-03-19 17:03:54 +0530

    update the files

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = intro;
 function intro() {
     console.log("Welcome To TypeScript Series\nBy Mayank Yadav\n");
 }
-exports.default = intro;
